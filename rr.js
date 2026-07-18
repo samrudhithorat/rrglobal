c691bf4c46c448124b913b85a6e61b5314c3eb04
samrudhithorat <samrudhithorat06@gmail.com>
2026-07-18 13:53:20 +0530

    Added About Us and Export pages

---
+++ rr.js
@@ -0,0 +1,33 @@
+function searchItem(){
+
+var search=document.getElementById("searchInput").value.toLowerCase();
+
+if(search=="export"){
+window.location="export.html";
+}
+
+else if(search=="import"){
+window.location="import.html";
+}
+
+else if(search=="services"){
+window.location="services.html";
+}
+
+else if(search=="about"){
+window.location="about.html";
+}
+
+else if(search=="contact"){
+window.location="contact.html";
+}
+
+else if(search=="login"){
+window.location="login.html";
+}
+
+else{
+alert("No Result Found");
+}
+
+}
