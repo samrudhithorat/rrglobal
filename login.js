4b13b9b8dca378a506321113f6727384c7ea72e9
samrudhithorat <samrudhithorat06@gmail.com>
2026-07-18 19:21:58 +0530

    Updated website design

---
+++ login.js
@@ -0,0 +1,19 @@
+
+function searchBox(){
+
+let box=document.getElementById("search");
+
+
+if(box.style.display=="block")
+{
+box.style.display="none";
+}
+
+else
+{
+box.style.display="block";
+}
+
+}
+
+
